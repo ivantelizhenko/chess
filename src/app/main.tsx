f0d80c03194e674ba0b03dd50786aea5a8b0f033
ivantelizhenko <telizenkoo@gmail.com>
2025-05-09 10:57:08 +0300

    додав supabase

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,12 +5,15 @@ import { Provider } from 'react-redux';
 import App from './App.tsx';
 import GlobalStyles from '../styles/GlobalStyles.ts';
 import store from '../store/store.ts';
+import SupabaseProvider from '../context/SupabaseContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    <GlobalStyles />
+    <SupabaseProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <GlobalStyles />
+    </SupabaseProvider>
   </StrictMode>
 );
