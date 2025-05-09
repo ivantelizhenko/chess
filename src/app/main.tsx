6ac305acd152d8c7d8a1cfd08f174feb3280d87e
ivantelizhenko <telizenkoo@gmail.com>
2025-05-09 12:22:04 +0300

    інтегрував react query

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,17 +3,22 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App.tsx';
-import GlobalStyles from '../styles/GlobalStyles.ts';
 import store from '../store/store.ts';
 import SupabaseProvider from '../context/SupabaseContext.tsx';
+import GlobalStyles from '../styles/GlobalStyles.ts';
+import ReactQueryProvider from '../components/ReactQueryProvider.tsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SupabaseProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-      <GlobalStyles />
-    </SupabaseProvider>
+    <Provider store={store}>
+      <ReactQueryProvider>
+        <SupabaseProvider>
+          <GlobalStyles />
+          <App />
+        </SupabaseProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ReactQueryProvider>
+    </Provider>
   </StrictMode>
 );
